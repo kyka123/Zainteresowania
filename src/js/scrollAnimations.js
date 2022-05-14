e1bf404c6be77fb45856b53cce781616e895f88d
kyka123 <kyka123@op.pl>
2022-05-14 14:37:00 +0200

    Style footer, add scroll animations to sections

--- src/js/scrollAnimations.js
+++ src/js/scrollAnimations.js
@@ -1,6 +1,15 @@
 const pageHeight = window.innerHeight;
+const scrollMaxY =
+  window.scrollMaxY ||
+  document.documentElement.scrollHeight - document.documentElement.clientHeight;
+
+const limit = scrollMaxY / 8;
+
+gsap.registerPlugin(ScrollTrigger);
 
 gsap.defaults({ ease: "expo.inOut" });
+
+console.log(scrollMaxY);
 
 gsap.to(".main__background", {
   scrollTrigger: {
@@ -9,7 +18,6 @@ gsap.to(".main__background", {
     toggleActions: "play none none reverse",
   },
   width: "100%",
-  ease: "expo.inOut",
   duration: 0.7,
 });
 
@@ -19,8 +27,63 @@ gsap.to(".main", {
     trigger: "body",
     toggleActions: "play none none reverse",
   },
-  ease: "expo.inOut",
   y: -pageHeight,
 
   duration: 0.7,
+});
+
+gsap.to("#section-1", {
+  scrollTrigger: {
+    start: `${limit}px`,
+    end: `+=${2 * limit}px`,
+    toggleActions: "play none none reverse",
+    // scrub: 0.6,
+    markers: true,
+  },
+  // ease: "none",
+  duration: 0.7,
+  autoAlpha: 0,
+});
+
+gsap.from("#section-2", {
+  scrollTrigger: {
+    start: `${limit}px`,
+    end: `+=${2 * limit}px`,
+    toggleActions: "play none none reverse",
+    // scrub: 0.6,
+    markers: true,
+  },
+  // ease: "none",
+
+  duration: 0.7,
+
+  yPercent: 100,
+});
+
+gsap.to("#section-2", {
+  scrollTrigger: {
+    start: `${2 * limit}px`,
+    end: `+=${3 * limit}px`,
+    toggleActions: "play none none reverse",
+    // scrub: 0.6,
+    markers: true,
+  },
+  // ease: "none",
+  duration: 0.7,
+
+  autoAlpha: 0,
+});
+
+gsap.from("#section-3", {
+  scrollTrigger: {
+    start: `${2 * limit}px`,
+    end: `+=${3 * limit}px`,
+    toggleActions: "play none none reverse",
+    // scrub: 0.6,
+    markers: true,
+  },
+  // ease: "none",
+  duration: 0.7,
+
+  yPercent: 100,
 });
